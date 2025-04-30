ac03eb01c30b55587f2710b048628c05de30614d
MJ <168153844+supremeysi@users.noreply.github.com>
2025-04-30 23:37:10 +0800

    Add ContactUsSection component

---
+++ capstone_web/resources/js/components/home_sections/ContactUsSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactUsSection: React.FC = () => {
+  return (
+    <div className="p-10 bg-green-400 text-white text-2xl text-center">
+      CONTACT SECTION IS WORKING
+    </div>
+  );
+};
+
+export default ContactUsSection;
+
