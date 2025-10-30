a1134210ecc106d1774fa78b346dc1e96537bf54
Mariagrasyaa <mariagraciavillavicencio20@gmail.com>
2025-10-30 17:25:21 +0800

    Fix the routing of PickOrder

---
+++ capstone_kiosk/resources/js/Pages/kiosk_pages/PickDineOrTake.jsx
@@ -0,0 +1,13 @@
+// resources/js/Pages/kiosk_pages/PickDineOrTake.jsx
+import React from 'react';
+import PickOrder from '@/Components/welcomeState/PickOrder';
+
+const PickDineOrTake = () => {
+  return (
+    <>
+      <PickOrder />
+    </>
+  );
+};
+
+export default PickDineOrTake;
